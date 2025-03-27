808cd49ab1fa6dd335543861546ee69884a21b12
aaaaaabhishek <158762526+aaaaaabhishek@users.noreply.github.com>
2025-03-27 18:24:51 +0530

    Delete keyclock_rest/keyclock_rest directory

--- keyclock_rest/keyclock_rest/src/main/java/com/rest_api/keyclock_rest/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package com.rest_api.keyclock_rest.service;
-public interface RoleService {
-
-    void assignRole(String userId ,String roleName);
-    void deleteRoleFromUser(String userId ,String roleName);
-
-}
-
