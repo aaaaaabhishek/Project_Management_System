6a0618b99a7e862f2d2f06695fbccc29b4d7022c
aaaaaabhishek <158762526+aaaaaabhishek@users.noreply.github.com>
2025-03-31 16:26:17 +0530

    Add files via upload

---
+++ projectmanagement/service/RoleService.java
@@ -0,0 +1,8 @@
+package com.modus.projectmanagement.service;
+public interface RoleService {
+
+    void assignRole(String userId ,String roleName);
+    void deleteRoleFromUser(String userId ,String roleName);
+
+}
+
